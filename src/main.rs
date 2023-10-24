951abe79360318dfa54b238f8a25ba2b49ccc57a
Daniel R <nzdanron@gmail.com>
2023-10-24 23:36:12 +1300

    Gets all repos now
    Added the --limit flag with a super high number to get all repos a user has. Also moved the code into functions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use std::process::Command;
+use std::process::Output;
 
 fn main() {
-    let output = Command::new("gh").arg("repo").arg("list").output();
+    let output = get_repos_raw();
 
-    if let Ok(output) = output {
-        let output_string = String::from_utf8_lossy(&output.stdout);
-        output_string
+    for repo in extract_repos(output) {
+        println!("{}", repo);
+    }
+}
+
+/// Exectures the gh repo list command and returns the result as an option
+fn get_repos_raw() -> Option<Output> {
+    Command::new("gh")
+        .arg("repo")
+        .arg("list")
+        .arg("--limit")
+        .arg("1000")
+        .output()
+        .ok()
+}
+
+/// Gets the list of repos from the "gh repo list" command output
+fn extract_repos(output: Option<Output>) -> Vec<String> {
+    match output {
+        Some(output) => String::from_utf8_lossy(&output.stdout)
             .split_whitespace()
             .filter(|x| x.contains('/'))
-            .for_each(|repo_name| println!("{}", repo_name));
+            .map(|x| x.to_owned())
+            .collect(),
+        None => vec![],
     }
 }
