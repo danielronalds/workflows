90dd72654974fc6ec13420bd4688df0e717cb16c
Daniel R <nzdanron@gmail.com>
2023-10-24 23:57:19 +1300

    extract_repos() method now returns a collection of Repo structs
    Created a parser that converts the output of the gh repo list command
    into a collection of Repo structs. The Repo struct has two fields, a
    name and a link to the git repo.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let output = get_repos_raw();
 
     for repo in extract_repos(output) {
-        println!("{}", repo);
+        println!("NAME: {}, \nURL: {}\n", repo.name, repo.url);
     }
 }
 
@@ -20,14 +20,38 @@ fn get_repos_raw() -> Option<Output> {
         .ok()
 }
 
+struct Repo {
+    name: String,
+    url: String,
+}
+
+impl Repo {
+    pub fn new<T: Into<String>>(name: T, url: T) -> Self {
+        let name = name.into();
+        let url = format!("https://www.github.com/{}.git", url.into());
+        Self { name, url }
+    }
+}
+
 /// Gets the list of repos from the "gh repo list" command output
-fn extract_repos(output: Option<Output>) -> Vec<String> {
-    match output {
-        Some(output) => String::from_utf8_lossy(&output.stdout)
+fn extract_repos(output: Option<Output>) -> Vec<Repo> {
+    if let Some(output) = output {
+        let repo_strings: Vec<String> = String::from_utf8_lossy(&output.stdout)
             .split_whitespace()
             .filter(|x| x.contains('/'))
             .map(|x| x.to_owned())
-            .collect(),
-        None => vec![],
+            .collect();
+
+        let repos: Vec<Repo> = repo_strings
+            .iter()
+            .filter_map(|repo_string| {
+                let name = repo_string.split('/').nth(1);
+                name.map(|name| Repo::new(name, repo_string))
+            })
+            .filter(|repo| !repo.name.is_empty())
+            .collect();
+
+        return repos;
     }
+    vec![]
 }
