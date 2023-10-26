9c1187edeff94ca8f80c6298a365dd902de13abd
Daniel R <nzdanron@gmail.com>
2023-10-26 16:10:32 +1300

    Refactored code
    - Removed get_repo_raw function, folded it into the one function
    - Renamed extract_repos functino to get_users_repos to reflect its new functionality
    - Changed main function to use filter map

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::fs;
 use std::process::Command;
-use std::process::Output;
 
 mod repo;
 use repo::Repo;
 
 fn main() {
-    let git_repos = extract_repos(get_repos_raw());
+    let git_repos = get_users_repos();
 
     let projects = get_current_projects();
 
     let local_projects: Vec<Repo> = git_repos
         .iter()
-        .filter(|x| projects.contains(&x.name()))
-        .map(|x| x.to_owned())
+        .filter_map(|repo| {
+            if projects.contains(&repo.name()) {
+                return None;
+            }
+            Some(repo.to_owned())
+        })
         .collect();
 
     for local_project in local_projects {
@@ -21,6 +24,11 @@ fn main() {
     }
 }
 
+/// Gets the projects currently in ~/Projects/
+///
+/// # Returns
+///
+/// A vec of strings containing the names of the directories in the project folder
 fn get_current_projects() -> Vec<String> {
     let entries = fs::read_dir("/home/danielr/Projects/").unwrap();
     let directories: Vec<String> = entries
@@ -35,19 +43,20 @@ fn get_current_projects() -> Vec<String> {
     return directories;
 }
 
-/// Exectures the gh repo list command and returns the result as an option
-fn get_repos_raw() -> Option<Output> {
-    Command::new("gh")
+/// Gets the list of repos from the "gh repo list" command output
+///
+/// # Returns
+///
+/// A vec of repo structs
+fn get_users_repos() -> Vec<Repo> {
+    let output = Command::new("gh")
         .arg("repo")
         .arg("list")
         .arg("--limit")
         .arg("1000")
         .output()
-        .ok()
-}
+        .ok();
 
-/// Gets the list of repos from the "gh repo list" command output
-fn extract_repos(output: Option<Output>) -> Vec<Repo> {
     if let Some(output) = output {
         let repo_strings: Vec<String> = String::from_utf8_lossy(&output.stdout)
             .split_whitespace()
@@ -66,5 +75,6 @@ fn extract_repos(output: Option<Output>) -> Vec<Repo> {
 
         return repos;
     }
+
     vec![]
 }
