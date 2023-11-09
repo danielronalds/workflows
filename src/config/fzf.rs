caa0a998808b0a46e1f4bbd559d15ab7ebcb3138
Daniel R <nzdanron@gmail.com>
2023-11-10 09:08:25 +1300

    test: Add tests for `FzfConfig`
    Added testing for `FzfConfig`, ensuring that the configuration does what
    I expect it to

--- src/config/fzf.rs
+++ src/config/fzf.rs
@@ -2,6 +2,8 @@
 
 use serde::Deserialize;
 
+const DEFAULT_REVERSE_LAYOUT: bool = true;
+
 #[derive(Debug, Deserialize, Default, Clone, PartialEq, Eq)]
 pub struct FzfConfig {
     /// Whether fzf should have the reverse layout
@@ -16,6 +18,38 @@ impl FzfConfig {
     ///
     /// Default: `true`
     pub fn reverse_layout(&self) -> bool {
-        self.reverse_layout.unwrap_or(true)
+        self.reverse_layout.unwrap_or(DEFAULT_REVERSE_LAYOUT)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::{WorkflowsConfig, fzf::DEFAULT_REVERSE_LAYOUT};
+
+    #[test]
+    fn reverse_layout_dir_works() {
+        let toml = "\
+[fzf]
+reverse_layout = true";
+
+        let config: WorkflowsConfig = toml::from_str(toml).expect("Failed to unwrap toml");
+
+        assert_eq!(config.fzf().reverse_layout, Some(true))
+    }
+
+    #[test]
+    fn default_reverse_layout_dir_works() {
+        // The toml contains [fzf] so that the field is some on the WorkflowsConfig
+        // allowing the testing of the reverse_layout field to be `None`
+        let toml = "[fzf]";
+
+        let config: WorkflowsConfig = toml::from_str(toml).expect("Failed to unwrap toml");
+
+        assert_eq!(config.fzf.clone().unwrap().reverse_layout, None);
+
+        assert_eq!(
+            config.fzf().reverse_layout(),
+            DEFAULT_REVERSE_LAYOUT
+        )
     }
 }
