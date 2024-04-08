3efe80c4062116efff650fb8d8c36a8e5b1281a1
Daniel R <nzdanron@gmail.com>
2024-04-08 22:34:21 +1200

    docs: Add shorthand arguments to help menu
    Added the shorthand arguments to the help menu dialog

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -8,12 +8,12 @@ A cli tool for creating a seemless workflow with remote and local git repos.
 To open a project, run workflows with no arguments.
 
 Commands
---new       Creates a new project
---open      Opens a local project
---clone     Clones the git repo from the given URL and opens it using workflows
---delete    Deletes the given project from the local machine
---health    Checks that workflows can access the required programs
---help      Show this dialog
+  --new       -n   Creates a new project
+  --open      -o   Opens a local project
+  --clone     -c   Clones the git repo from the given URL and opens it using workflows
+  --delete    -d   Deletes the given project from the local machine
+  --health         Checks that workflows can access the required programs
+  --help      -h   Show this dialog
 ",
 env!("CARGO_PKG_VERSION"));
 
